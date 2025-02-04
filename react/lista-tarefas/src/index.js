c5efec55c36bd6c2d627c1af958bb752c94c2648
juanalenca <juanbarros.dev@gmail.com>
2025-02-04 12:11:07 -0300

    Criando um app com o React

---
+++ react/lista-tarefas/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Cria a raiz para renderização
+root.render(<App />);
